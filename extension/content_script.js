662db67be9958b0a052f9205ce72790c8b751c9f
David Buckley <david@davidbuckley.ca>
2022-08-20 17:10:27 -0400

    expand input searching

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,7 +1,27 @@
 console.log("Starting pmenu!");
 browser.runtime.onMessage.addListener((request) => {
+  const usernameCandidates = [
+    "input[autocomplete=username]",
+    "input[autocomplete=email]",
+    "input[type=email]",
+    "input[type=text]",
+  ];
+  const passwordCandidates = [
+    "input[type=password][autocomplete=current-password]",
+    "input[type=password][autocomplete=password]",
+    "input[type=password]",
+  ];
+ 
   // TODO: Try to figure out which one to use if there are multiple results
-  let passwordInput = document.querySelector("input[type=password]")
+  let passwordInput;
+  let usernameInput;
+
+  for (let query of passwordCandidates) {
+      passwordInput = document.querySelector(query);
+      if (passwordInput !== null) {
+          break;
+      }
+  }
   if (passwordInput === null) {
     console.log("can't find password field!")
     return;
@@ -9,8 +29,13 @@ browser.runtime.onMessage.addListener((request) => {
 
   let form = passwordInput.closest('form');
   if (form !== null) {
+      for (let query of usernameCandidates) {
+          usernameInput = form.querySelector(query);
+          if (usernameInput !== null) {
+              break;
+          }
+      }
     // TODO: Try to figure out which one to use if there are multiple results
-    let usernameInput = form.querySelector("input[type=text]");
     if (usernameInput !== null) {
       usernameInput.value = request.username;
     }
